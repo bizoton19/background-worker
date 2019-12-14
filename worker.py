2bcdcf61ffe68cb00108b25d71cc9bd2f05a3d54
asalomon-cpsc <asalomon@cpsc.gov>
2019-12-13 23:34:42 -0500

    commented env variables

--- worker.py
+++ worker.py
@@ -3,8 +3,8 @@ from azure.servicebus.control_client import ServiceBusService, Message, Topic
 #from pymongo import MOngoClient
 #connection_str = os.environ['SB_CONN_STR']
 #mongo_conn_str = os.environ['PROD_MONGODB']
-bus_service = ServiceBusService(
-    service_namespace="cpsc-topic.servicebus.windows.net", shared_access_key_name="RootManageSharedAccessKey", shared_access_key_value="FG76GE6eVG0HfdRFp9d7+opJMp4qhluotQIxAVpGmEw=")
+bus_service = ServiceBusService(shared_access_key_name="RootManageSharedAccessKey",
+                                shared_access_key_value="FG76GE6eVG0HfdRFp9d7+opJMp4qhluotQIxAVpGmEw=")
 topic_name = "on-prem-test"  # the topic name already defined in azure
 subscription = "heroku_consumer"  # unique name for this application/consumer
 bus_service.create_topic(topic_name)
