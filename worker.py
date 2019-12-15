d6454fe2c9469edf6a92077d63e6a5001966fdf2
asalomon-cpsc <asalomon@cpsc.gov>
2019-12-14 22:44:36 -0500

    added env variables

--- worker.py
+++ worker.py
@@ -1,10 +1,15 @@
 import os
 from azure.servicebus.control_client import ServiceBusService, Message, Topic
-#from pymongo import MOngoClient
-#connection_str = os.environ['SB_CONN_STR']
-#mongo_conn_str = os.environ['PROD_MONGODB']
-bus_service = ServiceBusService(service_namespace="cpsc-topic", shared_access_key_name="RootManageSharedAccessKey",
-                                shared_access_key_value="FG76GE6eVG0HfdRFp9d7+opJMp4qhluotQIxAVpGmEw=")
+# from pymongo import MongoClient
+# mongo_conn_str = os.environ['PROD_MONGODB']
+
+shared_access_key = os.getenv('shared_access_key')
+shared_access_value = os.getenv('shared_access_value')
+service_namespace = os.getenv('service_namespace')
+print(service_namespace)
+
+bus_service = ServiceBusService(service_namespace=service_namespace, shared_access_key_name=shared_access_key,
+                                shared_access_key_value=shared_access_value)
 topic_name = "on-prem-test"  # the topic name already defined in azure
 subscription = "heroku_consumer"  # unique name for this application/consumer
 bus_service.create_topic(topic_name)
@@ -17,5 +22,5 @@ msg = bus_service.receive_subscription_message(
 msg_body = msg.body.decode("utf-8")
 print(msg_body)
 # write to DB
-#mClient = MongoClient(mongo_conn_str)
+# mClient = MongoClient(mongo_conn_str)
 # get db
